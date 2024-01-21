d394244afc827d3ba45f62bf0cc9cc2d1251fe0a
Jesús Cova <jesuscova@jisparking.com>
2024-01-21 10:44:40 -0300

    cambios

--- resources/js/project/App.vue
+++ resources/js/project/App.vue
@@ -381,43 +381,43 @@ export default {
         this.sendForm();
       } else {
         if (!this.q1) {
-          console.error("la respuesta de la pregunta N° 1 es obligatoria por favor rellenarla.");
+          alert("la respuesta de la pregunta N° 1 es obligatoria por favor rellenarla.");
         }
 
         if (!this.q2) {
-          console.error("la respuesta de la pregunta N° 2 es obligatoria por favor rellenarla.");
+          alert("la respuesta de la pregunta N° 2 es obligatoria por favor rellenarla.");
         }
 
         if (!this.q3) {
-          console.error("la respuesta de la pregunta N° 3 es obligatoria por favor rellenarla.");
+          alert("la respuesta de la pregunta N° 3 es obligatoria por favor rellenarla.");
         }
 
         if (!this.q4) {
-          console.error("la respuesta de la pregunta N° 4 es obligatoria por favor rellenarla.");
+          alert("la respuesta de la pregunta N° 4 es obligatoria por favor rellenarla.");
         }
 
         if (!this.q5) {
-          console.error("la respuesta de la pregunta N° 5 es obligatoria por favor rellenarla.");
+          alert("la respuesta de la pregunta N° 5 es obligatoria por favor rellenarla.");
         }
 
         if (!this.q6) {
-          console.error("la respuesta de la pregunta N° 6 es obligatoria por favor rellenarla.");
+          alert("la respuesta de la pregunta N° 6 es obligatoria por favor rellenarla.");
         }
 
         if (!this.q7) {
-          console.error("la respuesta de la pregunta N° 7 es obligatoria por favor rellenarla.");
+          alert("la respuesta de la pregunta N° 7 es obligatoria por favor rellenarla.");
         }
 
         if (!this.q8_1) {
-          console.error("la respuesta de la pregunta N° 8 es obligatoria por favor rellenarla.");
+          alert("la respuesta de la pregunta N° 8 es obligatoria por favor rellenarla.");
         }
 
         if (!this.q8_2) {
-          console.error("la respuesta de la pregunta N° 8 es obligatoria por favor rellenarla.");
+          alert("la respuesta de la pregunta N° 8 es obligatoria por favor rellenarla.");
         }
 
         if (!this.q8_3) {
-          console.error("la respuesta de la pregunta N° 8 es obligatoria por favor rellenarla.");
+          alert("la respuesta de la pregunta N° 8 es obligatoria por favor rellenarla.");
         }
       }
     },
